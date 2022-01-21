fdf4d9776a5ed7cb591abda1238c0ab85fd5998d
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-21 14:10:34 +0800

    Added db connect file

---
+++ src/db/connect.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import config from "config";
+import log from "../logger";
+
+const connect = () => {
+  const dbURI = config.get("dbURI") as string;
+  const dbOptions = config.get("mongoOptions");
+
+  return mongoose
+    .connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => log.info("Database connected."))
+    .catch((e) => {
+      log.error("db error", e);
+      process.exit(1);
+    });
+};
+
+export default connect;
