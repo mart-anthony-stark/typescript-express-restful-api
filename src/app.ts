2875edbbec051085d2dfdeeca4aa64ff7c7842d1
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-21 14:10:25 +0800

    Added connect

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import config from "config";
+import log from "./logger";
+import connect from "./db/connect";
 
 const port = config.get("port") as number;
 const dbURI = config.get("dbURI") as string;
@@ -10,5 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
-  console.log(`Server started running at port ${port}`);
+  log.info(`Server started running at port ${port}`);
+  connect();
 });
